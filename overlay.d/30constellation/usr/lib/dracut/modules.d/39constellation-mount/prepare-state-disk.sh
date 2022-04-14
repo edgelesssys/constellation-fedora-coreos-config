26d0eb7520b196b2d418a2b135d2086012d0b546
Daniel Weiße <66256922+daniel-weisse@users.noreply.github.com>
2022-04-14 08:45:37 +0200

    Only resize state parititon if it is not already initialized (#9)
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- overlay.d/30constellation/usr/lib/dracut/modules.d/39constellation-mount/prepare-state-disk.sh
+++ overlay.d/30constellation/usr/lib/dracut/modules.d/39constellation-mount/prepare-state-disk.sh
@@ -9,7 +9,8 @@ panic () {
 
 # as a fallback - if no extra state disk is found - the CoreOS image has an extra partition with a fixed UUID that can hold encrypted state.
 # The UUID is set during image creation.
-STATEFUL_PART_UUID=57A7EF-57A7-57A7-57A7-57A7EF01
+STATEFUL_PART_UUID_UNITIALIZED=57A7EF01-57A7-57A7-57A7-57A7EF011000 # UUID of an unintialized partition
+STATEFUL_PART_UUID_INITIALIZED=57A7EF01-57A7-57A7-57A7-57A7EF011FFF # UUID of an initialized paritition
 STATEFUL_PART_LABEL=stateful
 STATEFUL_PART_NUMBER=6
 STATEFUL_DISK_BY_PARTLABEL="/dev/disk/by-partlabel/${STATEFUL_PART_LABEL}"
@@ -19,13 +20,20 @@ STATEFUL_DISK="/dev/$(lsblk -dno NAME "${STATEFUL_DISK_BY_PARTLABEL}")"
 # hack: google nvme udev rules are never executed. Create symlinks for the nvme devices manually.
 for nvmedisk in /dev/nvme0n+([0-9])
 do
-  /usr/lib/udev/google_nvme_id -s -d $nvmedisk || true
+  /usr/lib/udev/google_nvme_id -s -d "${nvmedisk}" || true
 done
 
-end_position=$(sgdisk -E "${ROOT_DISK}")
-sgdisk -d "${STATEFUL_PART_NUMBER}" -e -n "${STATEFUL_PART_NUMBER}:0:$(( $end_position - ($end_position + 1) % 8 ))" -c "${STATEFUL_PART_NUMBER}:stateful" -u "${STATEFUL_PART_NUMBER}:${STATEFUL_PART_UUID}" -t 1:0700 "${ROOT_DISK}" || panic
-partx -u "${ROOT_DISK}"
-partx -u "${STATEFUL_DISK}"
+# only resize the disk if it is not already initialized
+# we change the UUID of the partition to mark it as initialized
+IFS=" " read -r -a guid <<< "$(sgdisk -i ${STATEFUL_PART_NUMBER} "${ROOT_DISK}" | grep "Partition unique GUID")"
+if [[ "${guid[3]}" == "$STATEFUL_PART_UUID_UNITIALIZED" ]]
+then
+  end_position=$(sgdisk -E "${ROOT_DISK}")
+  sgdisk -d "${STATEFUL_PART_NUMBER}" -e -n "${STATEFUL_PART_NUMBER}:0:$(( $end_position - ($end_position + 1) % 8 ))" -c "${STATEFUL_PART_NUMBER}:stateful" -u "${STATEFUL_PART_NUMBER}:${STATEFUL_PART_UUID_INITIALIZED}" -t 1:0700 "${ROOT_DISK}" || panic
+  partx -u "${ROOT_DISK}"
+  partx -u "${STATEFUL_DISK}"
+fi
+
 # Prepare the encrypted volume by either initializing it with a random key or by aquiring the key from another coordinator.
 # Create symlink to encrypted state disk at /dev/disk/by-id/state-disk (for example: /dev/disk/by-id/state-disk -> /dev/nvme0n2).
 # Store encryption key (random or recovered key) in /run/cryptsetup-keys.d/state.key
