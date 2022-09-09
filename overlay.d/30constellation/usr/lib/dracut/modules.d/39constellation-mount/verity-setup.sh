1f8626e07ec3aa9b4f379d5f0fa0abf2deb4b700
Malte Poll <mp@edgeless.systems>
2022-09-09 14:09:33 +0200

    stricter initrd ordering (#37)

--- overlay.d/30constellation/usr/lib/dracut/modules.d/39constellation-mount/verity-setup.sh
+++ overlay.d/30constellation/usr/lib/dracut/modules.d/39constellation-mount/verity-setup.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-panic () {
-  sleep 5 # allow error messages to be printed
-  echo 1 > /proc/sys/kernel/sysrq || true
-  echo c > /proc/sysrq-trigger || true
-}
-
 # Extract dm-verity hash from kernel cmdline
 roothash=$(cat /proc/cmdline)
 roothash="${roothash##*verity.sysroot=}"
@@ -16,7 +10,7 @@ then
     echo "dm-verity roothash (kernel cmdline verity.sysroot) contains well formatted hash: ${roothash}"
 else
     echo "dm-verity roothash (kernel cmdline verity.sysroot) contains improperly formatted hash"
-    panic
+    exit 1
 fi
 
 rootdevicepath=/dev/disk/by-partlabel/root_raw
@@ -24,36 +18,36 @@ hashdevicepath=/dev/disk/by-partlabel/root_verity
 vsysrootmapper=/dev/mapper/vsysroot
 if ! [ -b "${rootdevicepath}" ]; then
   echo "verity-setup: Failed to find root_raw: ${rootdevicepath}" 1>&2
-  panic
+  exit 1
 fi
 if ! [ -b "${hashdevicepath}" ]; then
   echo "verity-setup: Failed to find root_verity: ${hashdevicepath}" 1>&2
-  panic
+  exit 1
 fi
 
 echo "will now attempt to open dm-verity device with: veritysetup open ${rootdevicepath} vsysroot ${hashdevicepath} ${roothash}"
-veritysetup --panic-on-corruption open -v "${rootdevicepath}" vsysroot "${hashdevicepath}" "${roothash}" || panic
+veritysetup --panic-on-corruption open -v "${rootdevicepath}" vsysroot "${hashdevicepath}" "${roothash}" || exit 1
 
 if [ ! -b $vsysrootmapper ]; then
   echo "veritysetup create did not result in ${vsysrootmapper} being created"
-  panic
+  exit 1
 else
   echo "veritysetup create did result in ${vsysrootmapper} being created"
 fi
 
-mkdir -p /sysroot || panic
+mkdir -p /sysroot || exit 1
 # /sysroot should not be mounted by anyone else already
-mountpoint -q /sysroot && panic
+mountpoint -q /sysroot && exit 1
 
 echo "Mounting ${vsysrootmapper} ($(realpath "${vsysrootmapper}")) to /sysroot"
-mount -o ro "${vsysrootmapper}" /sysroot || panic
+mount -o ro "${vsysrootmapper}" /sysroot || exit 1
 
 if mountpoint -q /sysroot
 then
    echo "mounting /sysroot succeeded"
 else
    echo "mounting /sysroot failed"
-   panic
+   exit 1
 fi
 
 # Ensure we do not have to wait for a disk with label=root
